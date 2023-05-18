4a5c45bc4e2e2355601ad58d15a1ef3e4fae0e3f
Travis <46567145+TravisRoad@users.noreply.github.com>
2023-05-18 16:19:46 +0800

    trivial

--- src/components/Readme.tsx
+++ src/components/Readme.tsx
@@ -5,7 +5,7 @@ export default function Readme() {
     <div className="w-full sm:max-w-lg max-w-xs bg-slate-100 rounded-lg mt-2 ">
       <details>
         <summary className="text-md font-bold p-2">README</summary>
-        <ol className="list-decimal list-outside pl-6 pb-2">
+        <ol className="list-decimal list-outside pl-6 pb-2 pr-2">
           <li>消息阅后即焚</li>
           <li>服务器不保存密文本身，只保存解密的钥匙</li>
           <li>10 分钟后删除钥匙，服务器不会持久化任何信息</li>
